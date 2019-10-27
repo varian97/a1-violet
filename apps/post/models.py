21b1efbb6b483a307bd5ed2fef5d22730d714c6b
Varian <varian.caesar@dekoruma.com>
2019-10-27 20:39:16 +0700

    add post model

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = 'post'
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
