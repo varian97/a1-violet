31dc16bb7e17c7443f30fd47d8c8cd8ac15c268b
Varian <varian.caesar@dekoruma.com>
2019-11-17 22:45:59 +0700

    procfile add release phase

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn violet.wsgi --log-file -
--- violet/settings.py
+++ violet/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+IS_DEVELOPMENT = os.environ.get('IS_DEVELOPMENT', True)
+DEBUG = IS_DEVELOPMENT
 
 ALLOWED_HOSTS = []
 
