85d64691c15a0e373fadb2d91c1e3a81f1a536de
varian97 <varian.caesar@dekoruma.com>
2020-10-31 13:48:54 +0700

    fix: fix debug mode in settings

--- violet/settings.py
+++ violet/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-IS_DEVELOPMENT = os.environ.get('IS_DEVELOPMENT', True)
-DEBUG = IS_DEVELOPMENT
+IS_DEVELOPMENT = os.environ.get('IS_DEVELOPMENT', 'True')
+DEBUG = IS_DEVELOPMENT == 'True'
 
 ALLOWED_HOSTS = []
 
